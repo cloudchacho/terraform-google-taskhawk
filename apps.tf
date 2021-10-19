fe277e8798ca901d10a0b5dd0e4d5bfa39b37b5d
Aniruddha Maru <maru@standard.ai>
2021-10-19 12:17:08 -0700

    Customize test duration

--- apps.tf
+++ apps.tf
@@ -17,6 +17,10 @@ module "apps" {
   queue_alarm_high_priority_high_message_count_threshold = each.value.high_priority_high_message_count_threshold
   queue_alarm_low_priority_high_message_count_threshold  = each.value.low_priority_high_message_count_threshold
   queue_alarm_bulk_high_message_count_threshold          = each.value.bulk_high_message_count_threshold
+  queue_alarm_test_duration                              = each.value.high_message_count_test_duration
+  queue_alarm_high_priority_test_duration                = each.value.high_message_count_high_priority_test_duration
+  queue_alarm_low_priority_test_duration                 = each.value.high_message_count_low_priority_test_duration
+  queue_alarm_bulk_test_duration                         = each.value.high_message_count_bulk_test_duration
 
   enable_firehose_all_messages                   = var.enable_firehose_all_messages || each.value.enable_firehose == true
   dataflow_freshness_alert_threshold             = var.dataflow_freshness_alert_threshold
